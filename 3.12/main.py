92d4726b8fc3ec96a3c01e0c36c0e01b502eddae
Matteo Simonetti <anonimo345423@gmail.com>
2024-04-11 19:29:47 +0200

    risorse ma deadlock

--- 3.12/main.py
+++ 3.12/main.py
@@ -83,7 +83,7 @@ class Shifts: # codice preso solo per idea, bisogna trasformare orari degli shif
 
 class Process:
     executed_nodes = {}  # make executed_nodes a dictionary of sets
-    def __init__(self, env, name, process_details, num, start_delay=0, resources, instance_type="default"):
+    def __init__(self, env, name, process_details, num, resources, start_delay=0, instance_type="default"):
         self.env = env
         self.name = name
         self.process_details = process_details
@@ -92,8 +92,8 @@ class Process:
         self.instance_type = instance_type
         self.num = num
         self.action = env.process(self.run())
-        self.resources = resources #questo campo mantiene la struttura che sta in diagbp.json di resources, quello dopo lo adatta per simpy
-        self.simpyResources = {res['name']: simpy.Resource(env, capacity=int(res['totalAmount'])) for res in resources} if resources else {}
+        #resources is a dict with name as key and a tuple made of simpy resource, cost and timetable.
+        self.resources = {res['name']: (simpy.Resource(env, capacity=int(res['totalAmount'])), res['costPerHour'], res['timetableName']) for res in resources} if resources else {}
         Process.executed_nodes[self.num] = set() 
 
     def printState(self, node, node_id, inSubProcess):
@@ -103,6 +103,16 @@ class Process:
             print(f"#{self.num}|{self.name} (inside subprocess): {node_id} ({node['name']}), type: {node['type']}, instance_type:{self.instance_type}. time: {self.env.now}.")
         else:
             print(f"#{self.num}|{self.name}: {node_id} ({node['name']}), type: {node['type']}, instance_type:{self.instance_type}. time: {self.env.now}.")
+
+    def request_resources(self, group):
+        try:
+            requests = [(resource, amount, self.resources[resource][0].request()) for resource, amount in group for _ in range(amount)]
+            yield self.env.all_of(req for _, _, req in requests)  # asks for all the resources in the group
+            return requests
+        except simpy.Interrupt: # This process was interrupted because another group of resources is available
+            for _, _, req in requests:
+                req.resource.release(req) 
+                req.cancel()
 
     def run(self):
         yield self.env.timeout(self.start_delay) #delay start because of arrival rate.
@@ -134,26 +144,44 @@ class Process:
                 while not all(prev_node in Process.executed_nodes[self.num] for prev_node in node['previous']):
                     yield self.env.timeout(1)
             taskTime=timeCalculator.convert_to_seconds(task_durations[node_id]) # task duration is used here, it is passed before to a converter that transforms the type/mean/arg1/arg2 to a value in seconds, this value the task duration is always different in each instance
+            
+            #resources zone
             taskNeededResources=task_resources[node_id]
-            #TODO: usa taskNeededResources (dict con: resourceName, amountNeeded, groupId) per richiesta risorse. Risorse totali in self.resources
-            grouped_resources = {}
-            for res in taskNeededResources:
-                if res['groupId'] not in grouped_resources:
-                    grouped_resources[res['groupId']] = []
-                grouped_resources[res['groupId']].append((res['resourceName'], int(res['amountNeeded'])))
-            for group in grouped_resources.values():
-                requests = [self.simpyResources[resource].request() for resource, amount in group for _ in range(amount)]
-                yield self.env.all_of(requests)
-            # end resources req
+            if taskNeededResources:
+                grouped_resources = {}
+                for res in taskNeededResources:
+                    if res['groupId'] not in grouped_resources:
+                        grouped_resources[res['groupId']] = []
+                    grouped_resources[res['groupId']].append((res['resourceName'], int(res['amountNeeded'])))
+                # Create a process for each group of resources
+                for group_id, resources in grouped_resources.items():
+                    resource_names = [resource[0] for resource in resources]
+                    print(node_id+"|Group ID: " + str(group_id) + ", Resources: " + ', '.join(resource_names)) 
+                processes = [self.env.process(self.request_resources(group)) for group in grouped_resources.values()]
+
+                # Wait until one of the processes finishes (i.e., one group has all its resources available)
+                done_condition = yield self.env.any_of(processes)
+                done_process = list(done_condition.events)[0]  # Get the process that finished
+                done_requests = done_process.value  # Get the requests from the done process
+                for resource, amount, req in done_requests:
+                    req.resource.release(req)
+                    print (node_id + "|Resource locked: " + resource + ", Amount: " + str(amount))  # Print
+                # Interrupt all other processes because a group of resources was taken
+                for process in processes:
+                    if process is not done_process and process.is_alive:
+                        process.interrupt()
+                # end resources req
 
             yield self.env.timeout(taskTime) 
             Process.executed_nodes[self.num].add(node_id)
             self.printState(node,node_id,printFlag)
             next_node_id = node['next'][0]
-
             # Release resources
-            for req in requests:
-                yield self.simpyResources[req.resource].release(req)
+            for resource, amount, req in done_requests:
+                req.resource.release(req)
+                print (node_id + "|Resource released: " + resource + ", Amount: " + str(amount))  # Print
+
+
 
             yield from self.run_node(next_node_id, subprocess_node)
 
@@ -248,7 +276,7 @@ def simulate_bpmn(bpmn_dict):
         for participant_id, participant in bpmn_dict['collaboration']['participants'].items():
             process_details = bpmn_dict['process_elements'][participant['processRef']]
             # for each instance a class Process is created:
-            Process(env, participant['name'], process_details, num=i+1, start_delay=delays[i], resources, instance_type=instance_type)
+            Process(env, participant['name'], process_details,i+1, resources, delays[i], instance_type)
 
         instance_count += 1
         if instance_count >= int(instance_types[instance_index]['count']):
